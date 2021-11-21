b8c9cddc57b341ce54d14a467540c6119b78b5bc
btnguyenPersonal <btnguyenPersonal@github.com>
2021-11-20 23:49:38 -0600

    pull files script in progress

---
+++ pullfiles.sh
@@ -0,0 +1,6 @@
+cp ~/.bashrc ./.bashrc
+cp ~/.xinitrc ./.xinitrc
+cp ~/.config/nvim/init.vim ./init.vim
+cp /bin/dwm/config.h ./config.h
+cp /bin/dwm/dwm.c ./dwm.c
+cp ~/.bash_profile ./.bash_profile
