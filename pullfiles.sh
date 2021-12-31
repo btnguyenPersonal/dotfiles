aa8c30887a9afaa230e7046600b5724ac8fce73c
btnguyenPersonal <btnguyenPersonal@github.com>
2021-12-31 09:37:58 -0600

    Fri Dec 31 09:37:58 AM CST 2021

--- pullfiles.sh
+++ pullfiles.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cp /home/ben/.zshrc ./ben/.zshrc
-cp /home/ben/.zprofile ./ben/.zprofile
-cp /home/ben/.config/nvim/init.vim ./nvim/init.vim
-cp /home/ben/.config/alacritty/alacritty.yml ./alacritty/alacritty.yml
-cp /home/ben/.config/nvim/coc-settings.json ./nvim/coc-settings.json
-cp /home/ben/.tmux.conf ./tmux/.tmux.conf
-cp /home/ben/.vimrc ./ben/.vimrc
+cp /home/ben/.zshrc /home/ben/git/dotfiles/ben/.zshrc
+cp /home/ben/.zprofile /home/ben/git/dotfiles/ben/.zprofile
+cp /home/ben/.config/nvim/init.vim /home/ben/git/dotfiles/nvim/init.vim
+cp /home/ben/.config/alacritty/alacritty.yml /home/ben/git/dotfiles/alacritty/alacritty.yml
+cp /home/ben/.config/nvim/coc-settings.json /home/ben/git/dotfiles/nvim/coc-settings.json
+cp /home/ben/.tmux.conf /home/ben/git/dotfiles/tmux/.tmux.conf
+cp /home/ben/.vimrc /home/ben/git/dotfiles/ben/.vimrc
