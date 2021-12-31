95128a2a0992b12ee2e4db03bc406be38674bee5
btnguyenPersonal <btnguyenPersonal@github.com>
2021-12-30 22:49:50 -0600

    using pushfiles script no longer messes up my font sizes

--- pushfiles.sh
+++ pushfiles.sh
@@ -4,6 +4,10 @@ cp ./ben/.zshrc /home/ben/.zshrc
 cp ./ben/.zprofile /home/ben/.zprofile
 cp ./ben/.vimrc /home/ben/.vimrc
 cp ./nvim/init.vim /home/ben/.config/nvim/init.vim
-cp ./alacritty/alacritty.yml /home/ben/.config/alacritty/alacritty.yml
+if [ $(hostname) = 'benGreenLaptop' ]; then
+  sed 's/size: 12/size: 8/' ./alacritty/alacritty.yml > /home/ben/.config/alacritty/alacritty.yml;
+else
+  sed 's/size: 8/size: 12/' ./alacritty/alacritty.yml > /home/ben/.config/alacritty/alacritty.yml;
+fi
 cp ./nvim/coc-settings.json /home/ben/.config/nvim/coc-settings.json
 cp ./tmux/.tmux.conf /home/ben/.tmux.conf
