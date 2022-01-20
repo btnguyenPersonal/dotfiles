61f15d432f55339eedf871c39698216c0b87c11d
btnguyenPersonal <btnguyenPersonal@github.com>
2022-01-19 18:10:23 -0600

    Wed Jan 19 06:10:23 PM CST 2022

--- pullfiles.sh
+++ pullfiles.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cp /home/ben/.zshrc /home/ben/git/dotfiles/ben/.zshrc
-cp /home/ben/.zprofile /home/ben/git/dotfiles/ben/.zprofile
-cp /home/ben/.config/nvim/init.vim /home/ben/git/dotfiles/nvim/init.vim
-cp /home/ben/.config/alacritty/alacritty.yml /home/ben/git/dotfiles/alacritty/alacritty.yml
-cp /home/ben/.config/nvim/coc-settings.json /home/ben/git/dotfiles/nvim/coc-settings.json
-cp /home/ben/.tmux.conf /home/ben/git/dotfiles/tmux/.tmux.conf
-cp /home/ben/.vimrc /home/ben/git/dotfiles/ben/.vimrc
+cp -v /home/ben/.zshrc /home/ben/git/dotfiles/ben/.zshrc
+cp -v /home/ben/.zprofile /home/ben/git/dotfiles/ben/.zprofile
+cp -v /home/ben/.config/nvim/init.vim /home/ben/git/dotfiles/nvim/init.vim
+cp -v /home/ben/.config/alacritty/alacritty.yml /home/ben/git/dotfiles/alacritty/alacritty.yml
+cp -v /home/ben/.config/nvim/coc-settings.json /home/ben/git/dotfiles/nvim/coc-settings.json
+cp -v /home/ben/.tmux.conf /home/ben/git/dotfiles/tmux/.tmux.conf
+cp -v /home/ben/.vimrc /home/ben/git/dotfiles/ben/.vimrc
--- pushfiles.sh
+++ pushfiles.sh
@@ -2,15 +2,15 @@
 cd ~/git/dotfiles
 git fetch
 git pull
-cp ./ben/.xinitrc /home/ben/.xinitrc
-cp ./ben/.zshrc /home/ben/.zshrc
-cp ./ben/.zprofile /home/ben/.zprofile
-cp ./ben/.vimrc /home/ben/.vimrc
-cp ./nvim/init.vim /home/ben/.config/nvim/init.vim
+cp -v ./ben/.xinitrc /home/ben/.xinitrc
+cp -v ./ben/.zshrc /home/ben/.zshrc
+cp -v ./ben/.zprofile /home/ben/.zprofile
+cp -v ./ben/.vimrc /home/ben/.vimrc
+cp -v ./nvim/init.vim /home/ben/.config/nvim/init.vim
 if [ $(hostname) = 'benGreenLaptop' ]; then
   sed 's/size: 12/size: 8/' ./alacritty/alacritty.yml > /home/ben/.config/alacritty/alacritty.yml;
 else
   sed 's/size: 8/size: 12/' ./alacritty/alacritty.yml > /home/ben/.config/alacritty/alacritty.yml;
 fi
-cp ./nvim/coc-settings.json /home/ben/.config/nvim/coc-settings.json
-cp ./tmux/.tmux.conf /home/ben/.tmux.conf
+cp -v ./nvim/coc-settings.json /home/ben/.config/nvim/coc-settings.json
+cp -v ./tmux/.tmux.conf /home/ben/.tmux.conf
