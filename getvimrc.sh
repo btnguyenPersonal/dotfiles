78e8b4680d87efd58880f70ad2d6f0105698e4de
btnguyenPersonal <peteraxefan@gmail.com>
2022-06-19 19:30:18 -0500

    changing setup command

--- getvimrc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-wget https://raw.githubusercontent.com/btnguyenPersonal/dotfiles/main/.vimrc > ~/.vimrc
-wget https://raw.githubusercontent.com/btnguyenPersonal/dotfiles/main/.tmux.conf > ~/.tmux.conf
-wget https://raw.githubusercontent.com/btnguyenPersonal/dotfiles/main/.zshrc > ~/.zshrc
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+curl https://raw.githubusercontent.com/btnguyenPersonal/dotfiles/main/.vimrc
+curl https://raw.githubusercontent.com/btnguyenPersonal/dotfiles/main/.tmux.conf
+curl https://raw.githubusercontent.com/btnguyenPersonal/dotfiles/main/.zshrc
