49103e8e8780d24b37d5d84296176cbdfbc16207
btnguyenPersonal <peteraxefan@gmail.com>
2022-06-19 19:44:42 -0500

    full setup without zshrc

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -o ~/.vimrc https://raw.githubusercontent.com/btnguyenPersonal/dotfiles/main/.vimrc
 curl -o ~/.tmux.conf https://raw.githubusercontent.com/btnguyenPersonal/dotfiles/main/.tmux.conf
-curl -o ~/.zshrc https://raw.githubusercontent.com/btnguyenPersonal/dotfiles/main/.zshrc
