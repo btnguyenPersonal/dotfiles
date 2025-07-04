69fd840bbd9fa98af8d14caf0a48fcb87cf6863c
btnguyenPersonal <peteraxefan@gmail.com>
2025-07-04 12:04:48 -0500

    recordings in own folder and fzf from top

--- scripts/recording.sh
+++ scripts/recording.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
 sudo mount /dev/sda1 /mnt
-sudo mv -v /mnt/RECORD/* ~/git/recordings
+mkdir -v ~/git/recordings/$DATE
+sudo mv -v /mnt/RECORD/* ~/git/recordings/$DATE
