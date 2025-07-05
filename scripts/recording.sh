659dcc0e750b28e3f2a6df188bd22ee5cb018c63
btnguyenPersonal <peteraxefan@gmail.com>
2025-07-05 15:10:53 -0500

    revert, don't need folders

--- scripts/recording.sh
+++ scripts/recording.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
 sudo mount /dev/sda1 /mnt
-mkdir -v ~/git/recordings/$DATE
-sudo mv -v /mnt/RECORD/* ~/git/recordings/$DATE
+sudo mv -v /mnt/RECORD/* ~/git/recordings
