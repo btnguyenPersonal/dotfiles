d3737f7a44c5052bf78ce81b725137eafc5c41b6
ben <ben@max.lan>
2025-11-06 20:33:46 -0600

    remove unnecessary

--- README.md
+++
@@ -1,5 +0,0 @@
-has my configs for my terminal environment
-
-```
-curl https://raw.githubusercontent.com/btnguyenPersonal/dotfiles/main/setup.sh
-```
--- sync.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cp -r ~/.local/scripts .
-cp ~/.muttrc .
-cp ~/.config/alacritty/alacritty.toml .
-cp ~/.bashrc .
-cp ~/.xinitrc .
-cp ~/.vimrc .
-cp ~/.tmux.conf .
